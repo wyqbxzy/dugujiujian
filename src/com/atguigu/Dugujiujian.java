117a88fcc0af5ee2169ac807c0bb82a6410a2822
令狐冲 <wyqbxzy@126.com>
2018-02-22 15:42:17 +0800

    update 02

--- src/com/atguigu/Dugujiujian.java
+++ src/com/atguigu/Dugujiujian.java
@@ -4,5 +4,10 @@ public class Dugujiujian {
 	private void mian() {
 		System.out.println("hello");
 		System.out.println("hello linghuchong");
+		System.out.println("hello linghuchong");
+		System.out.println("hello linghuchong");
+		System.out.println("hello linghuchong");
+		System.out.println("hello linghuchong");
+		System.out.println("hello linghuchong");
 	}
 }
