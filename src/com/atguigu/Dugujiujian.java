fc68798e015789efcf5f3b82c93c2add2401ad8c
令狐冲 <wyqbxzy@126.com>
2018-02-22 15:16:40 +0800

    update 01

--- src/com/atguigu/Dugujiujian.java
+++ src/com/atguigu/Dugujiujian.java
@@ -2,7 +2,7 @@ package com.atguigu;
 
 public class Dugujiujian {
 	private void mian() {
-		// TODO Auto-generated method stub
 		System.out.println("hello");
+		System.out.println("hello linghuchong");
 	}
 }
